a35533eb95d5f3c472cd96dce87c5b38f380bac3
xRave110 <xRave110@protonmail.com>
2022-02-17 18:21:53 +0100

    Dashboard, nfts, balances

--- project_js/main.js
+++ project_js/main.js
@@ -1,9 +1,5 @@
-/* Moralis init code */
-const serverUrl = ${{ secrets.MORALIS_SERVER_URL }};
-const appId = ${{ secrets.API_ID }};
-Moralis.start({ serverUrl, appId });
-const user = Moralis.User.current();
 
+/* Moralis init code */
 const blockchains = {
     ETH: {
         chain: "eth",
@@ -38,6 +34,14 @@ const blockchains = {
     }
 };
 
+const envJson = JSON.parse(data);
+const serverUrl = envJson[1]["SERVER_URL"];
+const appId = envJson[0]["API_ID"];
+Moralis.start({ serverUrl, appId });
+const user = Moralis.User.current();
+
+getDashboard(blockchains.ETH);
+
 if(user == null && (!window.location.href.includes("/index.html"))){
     document.querySelector('body').style.display = 'none';
     window.location.href = "../index.html";
@@ -50,11 +54,19 @@ else if(user != null && !window.location.href.includes("/index.html")){
     console.log(`${user.get("ethAddress").substr(0, 6)}...${user.get("ethAddress").substr(-4, 4)}`);
 }
 
+
+async function fetchCoinList (){
+    let retCoins = await (await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false")).json();
+    //getDashboard(blockchains.ETH);
+
+    return retCoins;
+}
+
 /* Authentication code */
 async function login(email, password) {
 let user = Moralis.User.current();
     if (!user) {
-        user = await Moralis.authenticate({ signingMessage: "Log in to my page!!:)" }) // Message which appear in MetaMask
+        user = await Moralis.authenticate({ signingMessage: "Log in to the dashboard area"}) // Message which appear in MetaMask
         .then(async function (user) {
             console.log("logged in user:", user);
             console.log(user.get("ethAddress"));
@@ -102,8 +114,64 @@ async function logOut() {
     window.location.href = "../index.html";
 }
 
+async function getDashboard(blockchain) {
+    document.getElementById("content-table").innerHTML = ``;
+    document.getElementById("get-dashboard-link").setAttribute("class", "nav-link active");
+    document.getElementById("get-transactions-link").setAttribute("class", "nav-link");
+    document.getElementById("get-balances-link").setAttribute("class", "nav-link");
+    document.getElementById("get-nfts-link").setAttribute("class", "nav-link");
+    const options = { chain: blockchain.chain, address: user.get("ethAddress")};
+    const nativeBalance = await Moralis.Web3API.account.getNativeBalance(options);
+    const tokenBalances = await Moralis.Web3API.account.getTokenBalances(options);
+    const coins = await fetchCoinList();
+    //console.log(coins);
+    let coinsSymbolsToValue;
+    let coinsSymbolsToPrice = await coins.map(coin => `"${coin.symbol.toLowerCase()}": ${coin.current_price}` );
+    coinsSymbolsToPrice = JSON.parse(`{${coinsSymbolsToPrice}}`);
+    let coinsToActualValue;
+    let tokenPrices;
+    //console.log(Object.keys(coinsSymbolsToPrice));
+    //console.log(tokenBalances.map(token => token.symbol));
+    coinsToActualValue = tokenBalances
+                    .filter((token) => Object.keys(coinsSymbolsToPrice)
+                    .includes(token.symbol.toLowerCase()))
+                    .map(itoken => `"${itoken.symbol.toLowerCase()}": ${(itoken.balance / 1e18).toFixed(5)*coinsSymbolsToPrice[`${itoken.symbol.toLowerCase()}`]}`);
+
+    if(Object.keys(coinsSymbolsToPrice).includes(blockchain.ticker.toLowerCase())){
+        coinsToActualValue.push(`"${blockchain.ticker.toLowerCase()}": ${(nativeBalance.balance / 1e18).toFixed(5)*coinsSymbolsToPrice[`${blockchain.ticker.toLowerCase()}`]}`);
+    }
+    coinsToActualValue = JSON.parse(`{${coinsToActualValue}}`);
+    let totalAmount = 0;
+    Object.keys(coinsToActualValue).forEach((symbol) => {
+        totalAmount += +coinsToActualValue[symbol];
+    });
+    numbers = Object.values(coinsToActualValue).sort((a,b) => b - a);
+    console.log(numbers);
+
+    document.getElementById("content-table").innerHTML = `<h3>Total amount: ${totalAmount.toFixed(2)} $<h3>`
+    const listOfStyles = ["", "bg-success", "bg-info", "bg-warning", "bg-danger"];
+    let idx = 0;
+    Object.keys(coinsToActualValue).forEach(((symbol) => {
+        document.getElementById("content-table").innerHTML += `
+        <div d-flex>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped ${listOfStyles[idx]}" role="progressbar" style="width: ${(coinsToActualValue[symbol]/totalAmount)*100}%" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">${symbol.toUpperCase()} ${((coinsToActualValue[symbol]/totalAmount)*100).toFixed(2)}%</div>
+            </div>
+            <div>${coinsToActualValue[symbol].toFixed(5)} $</div>
+        </div>`;
+        idx++;
+    }))
+
+    
+}
+
 async function getTransactions(blockchain) {
-    document.getElementById("tableOfTransactions").innerHTML = '';
+    document.getElementById("title").innerHTML = `Transactions`;
+    document.getElementById("get-dashboard-link").setAttribute("class", "nav-link");
+    document.getElementById("get-transactions-link").setAttribute("class", "nav-link active");
+    document.getElementById("get-balances-link").setAttribute("class", "nav-link");
+    document.getElementById("get-nfts-link").setAttribute("class", "nav-link");
+    document.getElementById("content-table").innerHTML = '';
     const options = { chain: blockchain.chain, address: user.get("ethAddress")};
     const transactions = await Moralis.Web3API.account.getTransactions(options);
     console.log(transactions.result);
@@ -124,7 +192,7 @@ async function getTransactions(blockchain) {
         </tbody>
         </table>
         `
-        document.getElementById("tableOfTransactions").innerHTML = table;
+        document.getElementById("content-table").innerHTML = table;
         transactions.result.forEach(t => {
             let content = `
             <tr>
@@ -138,33 +206,88 @@ async function getTransactions(blockchain) {
             theTransaction.innerHTML += content;
         })
     }
+    else{
+        document.getElementById("content-table").innerHTML = `There are no transactions on ${blockchain.chain}`;
+    }
 }
 
-async function getBalances(){
-    
-    const options = { chain: "ropsten", address: user.get("ethAddress")};
-    const balance = await Moralis.Web3API.account.getNativeBalance(options);
-    console.log((balance.balance / 1e18).toFixed(5));
-    let content = document.getElementById('userBalances').innerHTML = `
+async function getBalances(blockchain){
+    document.getElementById("title").innerHTML = `Balances`;
+    document.getElementById("get-dashboard-link").setAttribute("class", "nav-link");
+    document.getElementById("get-transactions-link").setAttribute("class", "nav-link");
+    document.getElementById("get-balances-link").setAttribute("class", "nav-link active");
+    document.getElementById("get-nfts-link").setAttribute("class", "nav-link");
+    const options = { chain: blockchain.chain, address: user.get("ethAddress")};
+    const nativeBalance = await Moralis.Web3API.account.getNativeBalance(options);
+    const tokenBalances = await Moralis.Web3API.account.getTokenBalances(options);
+    document.getElementById('content-table').innerHTML = `
     <table class="table">
     <thead>
         <tr>
-            <th scope="col">Chain</th>
+            <th scope="col">Ticker</th>
             <th scope="col">Balance</th>
         </tr>
     </thead>
-    <tbody id="theTransaction">
+    <tbody id="theBalance">
         <tr>
             <td>
-                Ether
+                <strong>Chain: ${blockchain.chain.toUpperCase()}</strong>
             </td>
             <td>
-                ${(balance.balance / 1e18).toFixed(5)} ETH
+                <strong>Native Balance: ${(nativeBalance.balance / 1e18).toFixed(5)} ${blockchain.ticker}</strong>
             </td>
         </tr>
     </tbody>
     </table>
     `
+    tokenBalances.forEach( f => {
+        let content = 
+        `<tr>
+            <th> ${f.symbol} </th>
+            <th> ${(f.balance / 10**f.decimals).toFixed(5)} </th>
+        </tr>`
+        theBalance.innerHTML += content;
+    })
+}
+
+async function getNfts(blockchain){
+    document.getElementById("title").innerHTML = `NFTs`;
+    document.getElementById('content-table').innerHTML = ``;
+    document.getElementById("get-dashboard-link").setAttribute("class", "nav-link");
+    document.getElementById("get-transactions-link").setAttribute("class", "nav-link");
+    document.getElementById("get-balances-link").setAttribute("class", "nav-link");
+    document.getElementById("get-nfts-link").setAttribute("class", "nav-link active");
+    const options = { chain: blockchain.chain, address: user.get("ethAddress")};
+    const nfts = await Moralis.Web3API.account.getNFTs(options);
+    console.log(nfts);
+    if(nfts.result.length > 0){
+        document.getElementById('content-table').innerHTML = `<div id="nfts" class="d-flex flex-wrap"></div>`;
+        nfts.result.forEach(n => {
+            let metadata = JSON.parse(n.metadata);
+            if(metadata == null || typeof(metadata) != "object")
+            {
+                metadata = {"name": "None", "description": "None", "image": "None"};
+            }
+            if(!metadata.image.includes("https"))
+            {
+                metadata.image = "../images/null.jpg";
+            }
+            metadata.image = fixUrl(metadata.image);
+            
+            document.getElementById("nfts").innerHTML += 
+            `<div class="card col-md-2">
+                <img src=${metadata.image} class="card-img-top height=200">
+                <div class="card-body">
+                    <p>[${n.contract_type}]</p>
+                    <h6 class="card-title">${n.name} (${n.symbol})</h6>
+                    <p class="card-text">${metadata.description}</p>
+                </div>
+            </div>`;
+        })
+    }
+    else{
+        document.getElementById('content-table').innerHTML = `No NFTs for this chain`;
+    }
 }
 
 millisecondsToTime = (ms) => {
@@ -179,7 +302,32 @@ millisecondsToTime = (ms) => {
         } else return `${hours.toFixed(0)} hour(s) ago`;
     } else return `${days.toFixed(0)} day(s) ago`;
 }
+fixUrl = (url) => {
+    if(url.startsWith("ipfs")){
+        return "https://ipfs.moralis.io:2053/ipfs/" + url.split("ipfs://").slice(-1);
+    }
+    else{
+        return url + "?format=json";
+    }
+}
 
+function accountDetailsStates(blockchainSymbol){
+    if(document.getElementById("get-dashboard-link").getAttribute("class").includes("active")){
+        console.log("Dashboard");
+        getDashboard(blockchainSymbol);
+    }
+    else if(document.getElementById("get-transactions-link").getAttribute("class").includes("active")){
+        getTransactions(blockchainSymbol);
+    }
+    else if(document.getElementById("get-balances-link").getAttribute("class").includes("active")){
+        getBalances(blockchainSymbol);
+    }
+    else if(document.getElementById("get-nfts-link").getAttribute("class").includes("active")){
+        getNfts(blockchainSymbol);
+    }
+    
+}
+// Entrance
 if(document.getElementById("btn-login-meta") != null &&
     document.getElementById("btn-signup") != null &&
     document.getElementById("btn-login-email") != null)
@@ -191,48 +339,40 @@ if(document.getElementById("btn-login-meta") != null &&
     document.getElementById("btn-login-email").onclick = 
         () => logInEmail(document.getElementById("floatingInput").value, document.getElementById("floatingPassword").value); 
 }
-
 if(document.getElementById("btn-logout") != null){      
     document.getElementById("btn-logout").onclick = logOut;
 }
-
+// Get types
+if(document.getElementById("get-dashboard-link") != null){      
+    document.getElementById("get-dashboard-link").onclick = () => getDashboard(blockchains.ETH);
+}
 if(document.getElementById("get-transactions-link") != null){      
     document.getElementById("get-transactions-link").onclick = () => getTransactions(blockchains.ETH);
 }
-if(document.getElementById("eth-transactions") != null){      
-    document.getElementById("eth-transactions").onclick = () => getTransactions(blockchains.ETH);
-}
-if(document.getElementById("ropsten-transactions") != null){      
-    document.getElementById("ropsten-transactions").onclick = () => getTransactions(blockchains.ROPSTEN);
-}
-if(document.getElementById("bnb-transactions") != null){      
-    document.getElementById("bnb-transactions").onclick = () => getTransactions(blockchains.BNB);
-}
-if(document.getElementById("matic-transactions") != null){      
-    document.getElementById("matic-transactions").onclick = () => getTransactions(blockchains.MATIC);
-}
-if(document.getElementById("avx-transactions") != null){      
-    document.getElementById("avx-transactions").onclick = () => getTransactions(blockchains.AVX);
-}
-if(document.getElementById("ftm-transactions") != null){      
-    document.getElementById("ftm-transactions").onclick = () => getTransactions(blockchains.FTM);
-}
-/*
-"eth-transactions" cl
-"ropsten-transactions"
-"bnb-transactions" cl
-"matic-transactions" 
-"avx-transactions" cl
-"ftm-transactions" cl
-*/
-
 if(document.getElementById("get-balances-link") != null){      
-    document.getElementById("get-balances-link").onclick = () => getBalances('ETH');
+    document.getElementById("get-balances-link").onclick = () => getBalances(blockchains.ETH);
 }
 if(document.getElementById("get-nfts-link") != null){      
-    document.getElementById("get-nfts-link").onclick = logOut;
+    document.getElementById("get-nfts-link").onclick = () => getNfts(blockchains.ETH);
 }
 
-//get-transactions-link
-//get-balances-link
-//get-nfts-link
+
+// Chains to select
+if(document.getElementById("eth") != null){      
+    document.getElementById("eth").onclick = () => accountDetailsStates(blockchains.ETH);
+}
+if(document.getElementById("ropsten") != null){      
+    document.getElementById("ropsten").onclick = () => accountDetailsStates(blockchains.ROPSTEN);
+}
+if(document.getElementById("bnb") != null){      
+    document.getElementById("bnb").onclick = () => accountDetailsStates(blockchains.BNB);
+}
+if(document.getElementById("matic") != null){      
+    document.getElementById("matic").onclick = () => accountDetailsStates(blockchains.MATIC);
+}
+if(document.getElementById("avx") != null){      
+    document.getElementById("avx").onclick = () => accountDetailsStates(blockchains.AVX);
+}
+if(document.getElementById("ftm") != null){      
+    document.getElementById("ftm").onclick = () => accountDetailsStates(blockchains.FTM);
+}
